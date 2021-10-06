a5313bf6e5c3667df14f1fd5e2a9bf712f2f601e
Crnustes <cristiannustesm@gmail.com>
2021-10-06 09:44:36 -0500

    Conexion de formulario

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function sentMail(params) {
+    var tempParams = {
+        from_name: document.getElementById("toName").value,
+        to_name: document.getElementById("toEmail").value,
+        message: document.getElementById("toAsunto").value,
+        evento: document.getElementById("toService").value,
+        telefono: document.getElementById("toNumber").value,
+        comentario: document.getElementById("toComent").value,
+    }
+
+    emailjs.send('service_u3c243x', 'template_63a3xvg', tempParams)
+        .then(function (res) {
+            console.log("sucess", res.status);
+
+        })
+}
