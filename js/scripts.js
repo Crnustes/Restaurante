bfbbadb409e25e8eb2a922a0fcf591bbe4c6b403
Crnustes <cristiannustesm@gmail.com>
2021-10-06 15:26:23 -0500

    Cambios para carrito

--- js/scripts.js
+++ js/scripts.js
@@ -21,25 +21,20 @@ function sendMail(params) {
 }
 
 function sendReservas(params) {
-    var tempParams = {
-        from_name: document.getElementById("toName").value,
-        correo: document.getElementById("toEmail").value,
-        message: document.getElementById("toAsunto").value,
-        evento: document.getElementById("toService").value,
-        telefono: document.getElementById("toNumber").value,
-        comentario: document.getElementById("toComent").value,
-        cantidad: document.getElementById("cantidad").value,
-        cantidad: document.getElementById("fecha").value,
+    var tempParams1 = {
+        from_name: document.getElementById("toNameR").value,
+        correo: document.getElementById("toEmailR").value,
+        evento: document.getElementById("toServiceR").value,
+        telefono: document.getElementById("toNumberR").value,
+        comentario: document.getElementById("toComentR").value,
+        cantidad: document.getElementById("cantidadR").value,
+        fecha: document.getElementById("fechaR").value,
     }
 
-    emailjs.send('service_u3c243x', 'template_63a3xvg', tempParams)
+    emailjs.send('service_u3c243x', 'template_ss9ekej', tempParams1)
         .then(function (res) {
             console.log("sucess", res.status);
-
-
-
-
         })
-    alert('Muchas gracias!! nos contactaremos pronto');
+    alert("Su reserva a sido enviada");
 
 }
