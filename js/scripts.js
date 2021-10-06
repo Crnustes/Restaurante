fa28359b156a5e16a1d8546a89d614890f700e18
Crnustes <cristiannustesm@gmail.com>
2021-10-06 11:48:16 -0500

    Documento reservas

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,27 @@ function sendMail(params) {
     alert('Muchas gracias!! nos contactaremos pronto');
 
 }
+
+function sendReservas(params) {
+    var tempParams = {
+        from_name: document.getElementById("toName").value,
+        correo: document.getElementById("toEmail").value,
+        message: document.getElementById("toAsunto").value,
+        evento: document.getElementById("toService").value,
+        telefono: document.getElementById("toNumber").value,
+        comentario: document.getElementById("toComent").value,
+        cantidad: document.getElementById("cantidad").value,
+        cantidad: document.getElementById("fecha").value,
+    }
+
+    emailjs.send('service_u3c243x', 'template_63a3xvg', tempParams)
+        .then(function (res) {
+            console.log("sucess", res.status);
+
+
+
+
+        })
+    alert('Muchas gracias!! nos contactaremos pronto');
+
+}
