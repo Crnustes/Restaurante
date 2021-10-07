aa23bcac12c26a94f37624926c548b395845c095
Crnustes <cristiannustesm@gmail.com>
2021-10-07 15:18:41 -0500

    Tienda nueva

--- js/cart.js
+++ js/cart.js
@@ -6,6 +6,7 @@ Clickbutton.forEach(btn => {
     btn.addEventListener('click', addToCarritoItem)
 })
 
+
 function addToCarritoItem(e) {
     const button = e.target
     const item = button.closest('.card')
@@ -22,13 +23,16 @@ function addToCarritoItem(e) {
     }
 
     addItemCarrito(newItem)
-
 }
+
 
 function addItemCarrito(newItem) {
+
     carrito.push(newItem)
+
     renderCarrito()
 }
+
 
 function renderCarrito() {
     tbody.innerHTML = ''
@@ -36,20 +40,83 @@ function renderCarrito() {
         const tr = document.createElement('tr')
         tr.classList.add('ItemCarrito')
         const Content = `
-        
-            <th scope="row">1</td>
-            <td class="table__productos">
-                <img src= ${item.img}>
-                <h6 class="title">${item.title}</h6>
-            </td>
-            <td class="table__precio">${item.precio}</td>
-            <td class="table__cantidad">
-                <input type="number" min="1" value=${item.cantidad}>
-                <button class="delete btn btn-danget">X</button>
-            </td>        
-            `
-
+    
+        <th scope="row">1</td>
+        <td class="table__productos">
+            <img src= ${item.img}>
+            <h6 class="title">${item.title}</h6>
+        </td>
+        <td class="table__precio">${item.precio}</td>
+        <td class="table__cantidad">
+            <input type="number" min="1" value=${item.cantidad}>
+            <button class="delete btn btn-danget">X</button>
+        </td> 
+    
+    `
         tr.innerHTML = Content;
         tbody.append(tr)
+
+        tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+        tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
     })
+    CarritoTotal()
+}
+
+function CarritoTotal() {
+    let Total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) => {
+        const precio = Number(item.precio.replace("$", ''))
+        Total = Total + precio * item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total $${Total}`
+    addLocalStorage()
+}
+
+function removeItemCarrito(e) {
+    const buttonDelete = e.target
+    const tr = buttonDelete.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for (let i = 0; i < carrito.length; i++) {
+
+        if (carrito[i].title.trim() === title.trim()) {
+            carrito.splice(i, 1)
+        }
+    }
+
+    const alert = document.querySelector('.remove')
+
+    setTimeout(function () {
+        alert.classList.add('remove')
+    }, 2000)
+    alert.classList.remove('remove')
+
+    tr.remove()
+    CarritoTotal()
+}
+
+function sumaCantidad(e) {
+    const sumaInput = e.target
+    const tr = sumaInput.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item => {
+        if (item.title.trim() === title) {
+            sumaInput.value < 1 ? (sumaInput.value = 1) : sumaInput.value;
+            item.cantidad = sumaInput.value;
+            CarritoTotal()
+        }
+    })
+}
+
+function addLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function () {
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if (storage) {
+        carrito = storage;
+        renderCarrito()
+    }
 }
